c179c5046d267958596d8a3846bfeff4f7831440
Samuel Geremew <samuelgeremew@Samuels-MBP.fios-router.home>
2019-02-18 23:14:53 -0500

    Renamed src.txt and imported pkgs

---
+++ src/text-classify.py
@@ -0,0 +1,8 @@
+import re
+import nltk
+import numpy as np
+from nltk.stem import *
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
